394c704e0fdb699ed780c865d6b1728da5dba028
Tatiana Olshevskaya <st.forb@gmail.com>
2023-08-20 09:19:06 +0300

    work with itemUser and page status

---
+++ project/src/page/status/components/itemUser.vue
@@ -0,0 +1,21 @@
+<template>
+    <li class="dictionary__item">
+        <span class="dictionary__key"> {{ title }} </span>
+        <span class="dictionary__value"> {{ data }} </span>
+    </li>
+</template>
+
+<script>
+import { toRefs, ref } from "vue";
+export default {
+    props: ["data", "title"],
+    setup(props) {
+        const { title } = toRefs(props);
+        const data = ref(props.data);
+        return { title, data };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
